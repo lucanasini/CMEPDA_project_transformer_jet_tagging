ed18ee42165459e8ea294163ac71471600f4acd0
o0Parzival0o <lucanas2003@gmail.com>
2026-04-16 21:13:48 +0200

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 autodoc_mock_imports = ["torch", "numpy"]
+autodoc_member_order = 'bysource'
 
 
 # -- Options for HTML output -------------------------------------------------
